af886bd9fb00d5ebe4ecf53b419b8e692edb1f4c
unknown <seehusen@medienpunkt.com>
2018-02-16 16:50:11 +0100

    #14: using iterations to check speed

--- tests/GPProfTester.dpr
+++ tests/GPProfTester.dpr
@@ -7,16 +7,25 @@ program GPProfTester;
 
 uses
   System.SysUtils,
+  System.Diagnostics,
   testUnit in 'testUnit.pas',
   testThreads in 'testThreads.pas',
   testMultiDefines in 'testMultiDefines.pas';
 
+const ITERATIONS = 1000000;
+var I : integer;
+    LWatch : TStopwatch;
 begin
   try
-    TestProcedure;
-    TestFunction;
-    TestFunctionNestedType;
+    LWatch := TStopwatch.StartNew;
+    for i := 0 to ITERATIONS do
+      TestProcedure;
+    for i := 0 to ITERATIONS do
+      TestFunction;
+    for i := 0 to ITERATIONS do
+      TestFunctionNestedType;
     TestThread();
+    Writeln('Needed '+LWatch.ElapsedMilliseconds.ToString + ' millis');
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
